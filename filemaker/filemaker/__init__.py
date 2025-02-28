d48309917785366857a9ec600a3903b0463282b5
Josh <Josh@arktci.com>
2025-02-28 17:29:52 -0500

    Fix provider info: update version and correct hook paths

--- filemaker/filemaker/__init__.py
+++ filemaker/filemaker/__init__.py
@@ -5,7 +5,7 @@ This provider enables interaction with FileMaker Cloud's OData API,
 including custom Cognito authentication.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.1.0"
 
 
 def get_provider_info():
@@ -19,11 +19,11 @@ def get_provider_info():
         "package-name": "arktci-airflow-provider-filemaker",
         "name": "FileMaker Cloud",
         "description": "Provider for FileMaker Cloud OData API integration, including custom Cognito authentication.",
-        "hook-class-names": ["airflow.providers.filemaker.hooks.filemaker.FileMakerHook"],
+        "hook-class-names": ["filemaker.hooks.filemaker.FileMakerHook"],
         "connection-types": [
             {
                 "connection-type": "filemaker",
-                "hook-class-name": "airflow.providers.filemaker.hooks.filemaker.FileMakerHook",
+                "hook-class-name": "filemaker.hooks.filemaker.FileMakerHook",
             }
         ],
         "versions": [__version__],
