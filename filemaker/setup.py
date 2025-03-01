4d6654e864a15d4b7cd816e6602187a16e0667e4
Josh <Josh@arktci.com>
2025-03-01 17:56:27 -0500

    Update version management to use hardcoded versions in all files

--- filemaker/setup.py
+++ filemaker/setup.py
@@ -2,17 +2,15 @@ import os
 import re
 import setuptools
 
-# Read version from version.py
-with open(os.path.join("src", "airflow", "providers", "filemaker", "version.py"), "r") as version_file:
-    version_content = version_file.read()
-    VERSION = re.search(r'__version__ = ["\']([^"\']+)["\']', version_content).group(1)
+# Version managed by bump2version
+VERSION = "1.1.2"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="arktci-airflow-provider-filemaker",
-    version=VERSION,
+    version="1.1.2",
     author="Josh Lipton @ ArkTCI",
     author_email="josh@arktci.com",
     description="Apache Airflow provider for FileMaker Cloud",
