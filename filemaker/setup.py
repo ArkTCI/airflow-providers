e46d4f0d171b81d0e3461b9a37a7b451ba1c835b
Josh <Josh@arktci.com>
2025-03-03 13:08:00 -0500

    Bump version: 2.1.1 → 3.0.0

--- filemaker/setup.py
+++ filemaker/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="arktci-airflow-provider-filemaker",
-    version="2.1.1",
+    version="3.0.0",
     author="Josh Lipton @ ArkTCI",
     author_email="josh@arktci.com",
     description="Apache Airflow provider for FileMaker Cloud",
