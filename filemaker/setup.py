562e94cfca6fb2cd75f6d001360cd22d42ecfbc2
Josh <Josh@arktci.com>
2025-03-11 18:58:56 -0400

    updates to fix issuse

--- filemaker/setup.py
+++ filemaker/setup.py
@@ -6,8 +6,4 @@ Most metadata is specified in pyproject.toml.
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
-        # This minimal setup file is only needed for compatibility with older tools
-        # All actual metadata is defined in pyproject.toml
-        entry_points={"apache_airflow_provider": ["provider_info=airflow.providers.filemaker:get_provider_info"]},
-    )
+    setuptools.setup()
