3700aa8968c27e843292ede966cce0da1b20eeb5
Josh <Josh@arktci.com>
2025-03-13 10:13:44 -0400

    updsted configurationsfor bumping the versions

--- filemaker/version.py
+++ filemaker/version.py
@@ -2,5 +2,6 @@
 Version information for the FileMaker Cloud provider.
 This file serves as the single source of truth for version information.
 """
-# Version managed by bump2version
-__version__ = "2.1.0"
+# Version managed by bump3version
+VERSION = "2.1.2"
+__version__ = VERSION
